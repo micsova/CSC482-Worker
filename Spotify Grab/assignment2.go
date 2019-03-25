8dc9a929f51cac5846586b487a871b8ee1adf37b
micsova <micseidon@gmail.com>
2019-03-25 09:16:47 -0400

    Fixed parsing and added DynamoDB functionality

--- Spotify Grab/assignment2.go
+++ Spotify Grab/assignment2.go
@@ -3,6 +3,10 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/dynamodb"
+	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbattribute"
 	"github.com/jamesPEarly/loggly"
 	"io/ioutil"
 	"net/http"
@@ -14,34 +18,54 @@ import (
 )
 
 type Artist struct {
-	Name string
+	Name string `json:"name"`
+	HREF string `json:"href"`
+	ID   string `json:"id"`
 }
 
 type Album struct {
-	Name    string
-	Artists []Artist
-	ID      string
-	HREF    string
+	Name    string   `json:"name"`
+	Artists []Artist `json:"artists"`
+	ID      string   `json:"id"`
+	HREF    string   `json:"href"`
 }
 
 type Track struct {
-	Name        string
-	Artists     []Artist
-	Album       Album
-	TrackNumber int
-	ID          string
-	HREF        string
+	Name     string   `json:"name"`
+	Artists  []Artist `json:"artists"`
+	Album    Album    `json:"album"`
+	TrackNum int      `json:"track_number"`
+	ID       string   `json:"id"`
+	HREF     string   `json:"href"`
 }
 
-type Playlist struct {
-	Name        string
-	Description string
-	Tracks      []Track
-	TotalTracks int
-	Followers   int
-	ID          string
-	HREF        string
+type Follower struct {
+	Total int `json:"total"`
 }
+
+//type Item struct {
+//	TrackList Track `json:"track"`
+//}
+//
+//type Tracks struct {
+//	Items []Item `json:"items"`
+//}
+
+type Playlist struct {
+	Name        string `json:"name"`
+	Description string `json:"description"`
+	TrackList   struct {
+		Items []struct {
+			Song Track `json:"track"`
+		} `json:"items"`
+	} `json:"tracks"`
+	//TrackList	Tracks		`json:"tracks"`
+	ID        string   `json:"id"`
+	HREF      string   `json:"href"`
+	Followers Follower `json:"followers"`
+}
+
+const endpoint = "https://api.spotify.com/v1/playlists/37i9dQZF1DX4JAvHpjipBk"
 
 func main() {
 	// Check for token
@@ -52,116 +76,112 @@ func main() {
 		client := &http.Client{}
 
 		//Get access token using refresh token
-		dat := url.Values{}
-		dat.Add("grant_type", "refresh_token")
-		dat.Add("refresh_token", "AQAYxcApQyHMgY5M1q9sBCKRYCXwEF6ez5kvNPlQD1Oyd6_6H0TAxwJLJnJZ5cQWWar47gQMr_06YqFTW0sCIYztQTt1XePiaPURAqZiuhU9eYYfAIBC7sXweODnJ1vIWEeTpA")
-		req, _ := http.NewRequest(http.MethodPost, "https://accounts.spotify.com/api/token", strings.NewReader(dat.Encode()))
-		req.Header.Add("Authorization", "Basic ZjNmOGQ3MWNiZDQ2NDYwNGExZTA0MTJlZGMxM2IzOGU6M2MwMGFlNmJjNjVkNGM4ZWE2YTY4YTFhYTE4NDVkY2Y=")
-		req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
-		res, _ := client.Do(req)
-		body, _ := ioutil.ReadAll(res.Body)
-		byt := []byte(string(body))
-		var data map[string]interface{}
-		if err := json.Unmarshal(byt, &data); err != nil {
-			fmt.Println(err)
-		}
-		token := data["access_token"].(string)
+		token := getToken(client)
 
 		//Use access token to make a request
-		endpoint := "https://api.spotify.com/v1/playlists/37i9dQZF1DX4JAvHpjipBk"
-		req, _ = http.NewRequest(http.MethodGet, endpoint, nil)
-		req.Header.Add("Authorization", "Bearer "+token)
-		req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
-		res, _ = client.Do(req)
-		body, _ = ioutil.ReadAll(res.Body)
+		playlist := getPlaylist(token, client)
 
-		//Parse the resulting JSON file
-		byt = []byte(string(body))
-		var file interface{}
-		if err := json.Unmarshal(byt, &file); err != nil {
-			fmt.Println(err)
-		}
-		//fmt.Println(file)    // string interface matches values to keys   //Gets keys/values inside overall "albums" object
-		f := file.(map[string]interface{})
-		var playlist Playlist
-		playlistName := f["name"].(string)
-		playlistDescription := f["description"].(string)
-		playlistFollowers := int(f["followers"].(map[string]interface{})["total"].(float64))
-		playlistID := f["id"].(string)
-		playlistHREF := f["href"].(string)
-		var playlistTracks []Track
-		for key, value := range file.(map[string]interface{})["tracks"].(map[string]interface{}) { //Iterate through tracks data
-			if key == "items" {
-				for _, item := range value.([]interface{}) { //Iterate through tracks
-					for k, v := range item.(map[string]interface{}) {
-						if k == "track" {
-							track := v.(map[string]interface{})
-							trackName := track["name"].(string)
-							trackID := track["id"].(string)
-							trackHREF := track["href"].(string)
-							trackNumber := int(track["track_number"].(float64))
-							var trackArtists []Artist
-							for _, art := range track["artists"].([]interface{}) {
-								artist := art.(map[string]interface{})
-								trackArtists = append(trackArtists, Artist{
-									Name: artist["name"].(string),
-								})
-							}
-							album := v.(map[string]interface{}) //Get album of track
-							albumID := album["id"].(string)
-							albumHREF := album["href"].(string)
-							albumName := album["name"].(string)
-							var albumArtists []Artist
-							for _, art := range album["artists"].([]interface{}) { //Iterate through artists
-								artist := art.(map[string]interface{})
-								albumArtists = append(albumArtists, Artist{ //Create list of artists for the album
-									Name: artist["name"].(string),
-								})
-							}
-							trackAlbum := Album{
-								ID:      albumID,
-								HREF:    albumHREF,
-								Name:    albumName,
-								Artists: albumArtists,
-							}
-							playlistTracks = append(playlistTracks, Track{
-								Name:        trackName,
-								Artists:     trackArtists,
-								Album:       trackAlbum,
-								TrackNumber: trackNumber,
-								ID:          trackID,
-								HREF:        trackHREF,
-							})
-						}
-					}
+		/*
+			//Print data locally
+			var sb strings.Builder
+			sb.WriteString("Name: " + playlist.Name + "\n")
+			sb.WriteString("Description: " + playlist.Description + "\n")
+			sb.WriteString("Followers: " + strconv.Itoa(playlist.Followers.Total) + "\n")
+			sb.WriteString("Tracks: (" + strconv.Itoa(len(playlist.SongList.Items.Tracks)) + ")\n")
+			for _, song := range playlist.SongList.Items.Tracks {
+				sb.WriteString("    " + song.Name + " by ")
+				for _, artist := range song.Artists {
+					sb.WriteString(artist.Name + ", ")
 				}
+				sb.WriteString("\n")
 			}
-		}
-		playlist = Playlist{
-			Name:        playlistName,
-			Description: playlistDescription,
-			Tracks:      playlistTracks,
-			TotalTracks: len(playlistTracks),
-			Followers:   playlistFollowers,
-			ID:          playlistID,
-			HREF:        playlistHREF,
-		}
-		var sb strings.Builder
-		sb.WriteString("Name: " + playlist.Name + "\n")
-		sb.WriteString("Description: " + playlist.Description + "\n")
-		sb.WriteString("Followers: " + strconv.Itoa(playlist.Followers) + "\n")
-		sb.WriteString("Tracks: (" + strconv.Itoa(playlist.TotalTracks) + ")\n")
-		for _, track := range playlist.Tracks {
-			sb.WriteString("    " + track.Name + " by ")
-			for _, artist := range track.Artists {
-				sb.WriteString(artist.Name + ", ")
-			}
-			sb.WriteString("\n")
-		}
-		fmt.Print(sb.String())
+			fmt.Print(sb.String())
+		*/
+
+		//Loggly Reporting
 		logglyClient = loggly.New("Data")
 		_ = logglyClient.Send("info", "{\n\"name\": "+playlist.Name+"\",\n"+
-			"\"followers\": "+strconv.Itoa(3087179)+"\n}")
-		time.Sleep(5 * time.Minute)
+			"\"followers\": "+strconv.Itoa(playlist.Followers.Total)+"\n}")
+
+		//Create a DynamoDB session
+		sess, err := session.NewSession(&aws.Config{
+			Region: aws.String("us-east-1")},
+		)
+
+		if err != nil {
+			fmt.Println("Error creating session:")
+			fmt.Println(err.Error())
+			os.Exit(1)
+		}
+		svc := dynamodb.New(sess)
+
+		//Put the playlist info into the DynamoDB table
+		table(playlist, svc)
+
+		//Wait 15 minutes before polling again
+		time.Sleep(15 * time.Minute)
 	}
+}
+
+func getToken(client *http.Client) string {
+	dat := url.Values{}
+	dat.Add("grant_type", "refresh_token")
+	dat.Add("refresh_token", "AQAYxcApQyHMgY5M1q9sBCKRYCXwEF6ez5kvNPlQD1Oyd6_6H0TAxwJLJnJZ5cQWWar47gQMr_06YqFTW0sCIYztQTt1XePiaPURAqZiuhU9eYYfAIBC7sXweODnJ1vIWEeTpA")
+	req, _ := http.NewRequest(http.MethodPost, "https://accounts.spotify.com/api/token", strings.NewReader(dat.Encode()))
+	req.Header.Add("Authorization", "Basic ZjNmOGQ3MWNiZDQ2NDYwNGExZTA0MTJlZGMxM2IzOGU6M2MwMGFlNmJjNjVkNGM4ZWE2YTY4YTFhYTE4NDVkY2Y=")
+	req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+	res, _ := client.Do(req)
+	body, _ := ioutil.ReadAll(res.Body)
+	stringFile := []byte(string(body))
+	var data map[string]interface{}
+	if err := json.Unmarshal(stringFile, &data); err != nil {
+		fmt.Println(err)
+	}
+	token := data["access_token"].(string)
+	return token
+}
+
+func getPlaylist(token string, client *http.Client) Playlist {
+	req, _ := http.NewRequest(http.MethodGet, endpoint, nil)
+	req.Header.Add("Authorization", "Bearer "+token)
+	req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+	res, _ := client.Do(req)
+	body, _ := ioutil.ReadAll(res.Body)
+
+	//Parse the resulting JSON file
+	stringFile := []byte(string(body))
+	//fmt.Println(string(stringFile))
+	var playlist Playlist
+	if err := json.Unmarshal(stringFile, &playlist); err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println("Got playlist")
+	return playlist
+}
+
+func table(playlist Playlist, svc *dynamodb.DynamoDB) {
+	// Add each item to Movies table:
+	av, err := dynamodbattribute.MarshalMap(playlist)
+
+	if err != nil {
+		fmt.Println("Got error marshalling map:")
+		fmt.Println(err.Error())
+		os.Exit(1)
+	}
+
+	// Create item in table Movies
+	input := &dynamodb.PutItemInput{
+		Item:      av,
+		TableName: aws.String("Spotify-New-Music-Playlist"),
+	}
+
+	_, err = svc.PutItem(input)
+
+	if err != nil {
+		fmt.Println("Got error calling PutItem:")
+		fmt.Println(err.Error())
+		os.Exit(1)
+	}
+
+	fmt.Println("Successfully added '", playlist.Name, "' (", playlist.Followers, ") to Spotify-New-Music-Playlist table")
 }
